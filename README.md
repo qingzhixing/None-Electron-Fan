c7c3dc68d8f4e998ff657c1cf46be0bd7a103efa
qingzhixing <qzsptmail@protonmail.com>
2025-02-02 22:25:04 +0800

    🌈 Alive Report指令

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 4. 配置 `.env.prod` , 填写超级用户等 `SUPERUSERS= ["QQ号_1", "QQ号_2"]`
 5. 运行 **Lagrange**
 6. 更改 Lagrange配置，设置通信端口为 `5703`. 若端口已被占用 (会显示套接字报错)，同步修改此项目配置和Lagrange配置到同一端口即可
-7. 运行 `nb run --reload-includes pyproject.toml`
+7. 运行 `nb run`
 
 ## 🌈NoneBot Documentation
 
--- fan-plugins/alive_report/__init__.py
+++ fan-plugins/alive_report/__init__.py
@@ -1,14 +1,23 @@
-from nonebot import get_plugin_config
+from nonebot import get_plugin_config, on_command
+from nonebot.rule import to_me
 from nonebot.plugin import PluginMetadata
 
 from .config import Config
 
 __plugin_meta__ = PluginMetadata(
     name="Alive Report",
-    description="",
-    usage="",
+    description="检测机器人存活状态",
+    usage="指令为 /alive 或 /活着吗 或 /alive-check, 需要 @机器人 或者 私聊 才可使用",
     config=Config,
 )
 
 config = get_plugin_config(Config)
 
+alive_check = on_command(
+    "alive", rule=to_me(), aliases={"活着吗", "alive-check"}, priority=10, block=False
+)
+
+
+@alive_check.handle()
+async def handle_alive_check():
+    await alive_check.send("💮存活! I'm Alive!")
