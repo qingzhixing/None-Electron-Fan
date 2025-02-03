313f3313c412a8e19ed0bb7b4b54b92c973cb5c1
qingzhixing <qzsptmail@protonmail.com>
2025-02-03 17:09:43 +0800

    🎃 补充插件元信息，准备发布

--- fan-plugins/nbnhhsh/__init__.py
+++ fan-plugins/nbnhhsh/__init__.py
@@ -1,7 +1,7 @@
 from nonebot import get_plugin_config, logger, on_regex
 from nonebot.plugin import PluginMetadata
 from nonebot.params import RegexStr
-import requests
+import httpx
 import re
 
 from .config import Config
@@ -9,14 +9,16 @@ from .config import Config
 __plugin_meta__ = PluginMetadata(
     name="[ 缩写翻译 ] 能不能好好说话 - nbnhhsh",
     description="能不能好好说话 - 使用神奇海螺api查询",
-    usage="xxxx是什么",
+    usage="[缩写英文]是什么|是啥|啥玩意",
     config=Config,
+    type="application",
+    extra={},
 )
 
 # 输出插件加载日志
 logger.info("插件 nbnhhsh 已加载")
 
-regular_expression = r"[a-z0-9]{2,}+(?:是什么|是啥|啥)"
+regular_expression = r"[a-z0-9]{2,}+(?:是什么|是啥)"
 
 config = get_plugin_config(Config)
 
@@ -29,7 +31,7 @@ nbnhhsh_macher = on_regex(
 async def handle_nbnhhsh(keyword: str = RegexStr()):
     logger.info(f"原始 keywordGroup: {keyword}")  # 添加这一行以检查原始 keywordGroup
 
-    keyword_string = keyword.replace("是什么", "").replace("是啥", "").replace("啥", "")
+    keyword_string = keyword.replace("是什么", "").replace("是啥", "")
     logger.info(f"nbnhhsh keyword: {keyword_string}")
     # 调用神奇海螺api查询
     result_dict = await query_nbnhhsh(keyword_string)
@@ -76,27 +78,23 @@ async def query_nbnhhsh(keyword: str) -> dict:
         return Guess[keyword]
 
     try:
-        # 如果有正在进行的请求，终止它
-        if hasattr(query_nbnhhsh, "_request"):
-            query_nbnhhsh._request.close()
-            query_nbnhhsh._request = None
-
         # 发送POST请求
-        query_nbnhhsh._request = requests.post(
-            NBNHHSH_API_URL + "guess",
-            json={"text": keyword},
-            headers={"content-type": "application/json"},
-            timeout=10,  # 设置超时时间
-        )
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                NBNHHSH_API_URL + "guess",
+                json={"text": keyword},
+                headers={"Content-Type": "application/json"},
+                timeout=10,  # 设置超时时间
+            )
 
-        # 解析响应
-        data = query_nbnhhsh._request.json()
-        if data and len(data) > 0:
-            Guess[keyword] = data[0]
-            return data[0]
-        else:
-            return {}
+            # 解析响应
+            data = response.json()
+            if data and len(data) > 0:
+                Guess[keyword] = data[0]
+                return data[0]
+            else:
+                return {}
 
-    except requests.exceptions.RequestException as e:
+    except httpx._exceptions.RequestError as e:
         logger.error(f"请求出错: {e}")
         return {}
